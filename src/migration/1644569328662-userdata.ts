b370ba5bf676105122b6b5d0d5a99b09aaa93edd
Asim Khan <Asim.Khan@extentia.com>
2022-02-15 18:33:03 +0530

    Added migration

---
+++ src/migration/1644569328662-userdata.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class userdata1644569328662 implements MigrationInterface {
+  name = 'userdata1644569328662';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "users" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "users"`);
+  }
+}
