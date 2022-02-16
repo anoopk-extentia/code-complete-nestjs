9d9a4b0a4da6cbee1394465d155a135355de70ef
anoopk-extentia <anoop.kumar@extentia.com>
2022-02-17 00:41:15 +0530

    Integrated db connection and heap checks to the health check module

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -3,6 +3,8 @@ import {
   HealthCheckService,
   HttpHealthIndicator,
   HealthCheck,
+  TypeOrmHealthIndicator,
+  MemoryHealthIndicator
 } from '@nestjs/terminus';
 
 @Controller('health')
@@ -10,6 +12,8 @@ export class HealthController {
   constructor(
     private health: HealthCheckService,
     private http: HttpHealthIndicator,
+    private db: TypeOrmHealthIndicator,
+    private memHealth: MemoryHealthIndicator
   ) {}
 
   @Get()
@@ -17,6 +21,20 @@ export class HealthController {
   check() {
     return this.health.check([
       () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+    ]);
+  }
+  @Get('/memory')
+  @HealthCheck()
+  checkMem() {
+    return this.health.check([
+      () => this.memHealth.checkHeap("memory", 150*1024*1024),
+    ]);
+  }
+  @Get('/db')
+  @HealthCheck()
+  checkDb() {
+    return this.health.check([
+      () => this.db.pingCheck('database'),
     ]);
   }
 }
