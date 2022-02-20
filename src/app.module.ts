3bf7f5e7d0a712fef3329e71c33d0f3cbfd430dc
Asim Khan <Asim.Khan@extentia.com>
2022-02-20 13:23:21 +0530

    Added Config folder changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,3 @@
-// app.module.ts
-
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,31 +6,18 @@ import { NameInsertedListener } from './listeners/name-inserted.listener';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { Name } from './names/entities/name.entity';
-import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health/health.controller';
 import { HttpModule } from '@nestjs/axios';
+import { typeOrmConfigAsync } from './config/typeorm.config';
 
 @Module({
   imports: [
     NamesModule,
     EventEmitterModule.forRoot(),
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(
-        process.env.POSTGRES_PORT ? process.env.POSTGRES_PORT : '3000',
-      ),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      entities: [Name, User],
-      //ssl: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
     HttpModule,
     TerminusModule,
     UsersModule,
