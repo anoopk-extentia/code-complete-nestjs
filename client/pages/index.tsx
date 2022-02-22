a1979d51f7f23ebc94035909dc9cafcc2278147c
fayez-baig <coderninja16@gmail.com>
2022-02-22 19:34:21 +0530

    created-next-app

---
+++ client/pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Next App</title>
+        <meta name="description" content="NEXT APP" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Hello World ;)
+        </h1>
+      </main>
+
+    </div>
+  )
+}
+
+export default Home
