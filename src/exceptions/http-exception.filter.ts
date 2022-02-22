f37c697ad26d7e294933e5c33a4d2cc4b2aa3fc2
Asim Khan <Asim.Khan@extentia.com>
2022-02-22 14:26:44 +0530

    Added custom validation exceptions

--- src/exceptions/http-exception.filter.ts
+++ src/exceptions/http-exception.filter.ts
@@ -17,6 +17,7 @@ export class HttpExceptionFilter implements ExceptionFilter {
       statusCode: status,
       timestamp: new Date().toISOString(),
       path: request.url,
+      message:exception.message
     });
   }
 }
