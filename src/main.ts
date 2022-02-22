39aa1699e1dab4f3a84345c1c91e1aabacaf352c
khanasim26 <khanasim26@gmail.com>
2022-02-22 14:50:53 +0530

    Added custom validation exceptions (#21)
    Co-authored-by: Asim Khan <Asim.Khan@extentia.com>

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { INestApplication, VersioningType } from '@nestjs/common';
+import { INestApplication, VersioningType , ValidationError,
+  ValidationPipe} from '@nestjs/common';
 import { HttpExceptionFilter } from './exceptions/http-exception.filter';
 import helmet from 'helmet';
+import { ValidationException } from './exceptions/validation.exception';
+import { ValidationFilter } from './exceptions/validation.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.enableVersioning({ type: VersioningType.URI, defaultVersion: '0' });
-  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new HttpExceptionFilter(), new ValidationFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      skipMissingProperties: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        const messages = errors.map(
+          (err) =>
+            `${err.property} has wrong value ${
+              err.value
+            }. Constrained by:${JSON.stringify(err.constraints)}`,
+        );
+        return new ValidationException(messages);
+      },
+    }),
+  );
   await attachOpenAPIDocumentation(app);
   await app.listen(process.env.PORT ? process.env.PORT : 3000);
 }
--- src/names/dto/create-name.dto.ts
+++ src/names/dto/create-name.dto.ts
@@ -1,8 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Length, IsString } from 'class-validator';
+
 export class CreateNameDto {
   @ApiProperty()
+  @Length(10, 20)
+  @IsString({ message: 'text must be string' })
   text: string;
 
-  @ApiProperty({ required: false })
-  role?: string;
+  @ApiProperty({ required: true })
+  @IsString({ always: true })
+  role: string;
 }
