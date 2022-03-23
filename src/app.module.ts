da041765cd38c267392b67433b91e609c9124e1e
Asim Khan <Asim.Khan@extentia.com>
2022-03-23 12:46:06 +0530

    Removed typeorm config and used the default connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,27 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NamesModule } from './names/names.module';
 import { NameInsertedListener } from './listeners/name-inserted.listener';
-import { ConfigModule } from '@nestjs/config';
+
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { UsersModule } from './users/users.module';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health/health.controller';
 import { HttpModule } from '@nestjs/axios';
-import { typeOrmConfigAsync } from './config/typeorm.config';
 import { PostGraphileModule } from 'postgraphile-nest';
 
 @Module({
   imports: [
-    PostGraphileModule.forRoot({
-      pgConfig: 'postgres://postgres:postgres@localhost/sessions',
-    }),
     NamesModule,
     EventEmitterModule.forRoot(),
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    PostGraphileModule.forRoot({
+      pgConfig: `postgres://${process.env.TYPEORM_USERNAME}:${process.env.TYPEORM_PASSWORD}@${process.env.TYPEORM_HOST}/${process.env.TYPEORM_DATABASE}`,
+    }),
+    TypeOrmModule.forRoot(),
     HttpModule,
     TerminusModule,
     UsersModule,
